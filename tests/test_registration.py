136aff4e6e8b26404b91273798ecdcd6a8008f69
Missmurk <missmur1012@gmail.com>
2026-05-29 21:23:36 +0300

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,22 +1,24 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 
 
 @pytest.mark.regression  # Добавили маркировку regression
 @pytest.mark.registration  # Добавили маркировку registration
-def test_successful_registration():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+def test_successful_registration(chromium_page: Page):
+        chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
         email_input.fill("user.name@gmail.com")
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+
+        username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
         username_input.fill("username")
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+
+        password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
         password_input.fill("password")
-        registration_button = page.get_by_test_id('registration-page-registration-button')
+
+        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
         registration_button.click()
-        dashboard_title_text = page.get_by_test_id('dashboard-toolbar-title-text')
+
+        dashboard_title_text = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
         expect(dashboard_title_text).to_be_visible()
         expect(dashboard_title_text).to_have_text("Dashboard")
